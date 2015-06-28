555d9b35bb28a2d635f097c9f860dff136c2f243
GawainLam <931581799@qq.com>
2015-06-28 16:56:01 +0800

    redirect contract
    findG_L 大于 小于
    findGe_L 大于等于 小于
    findG_Le 大于 小于等于
    findGe_Le 大于等于 小于等于

--- src/com/mangement/mybatis/connections/Connect2Contract.java
+++ src/com/mangement/mybatis/connections/Connect2Contract.java
@@ -53,6 +53,95 @@ public class Connect2Contract extends SessionOpener {
 		}
 	}
 
+	public List<Contract> findG_L(Contract model,Integer start,Integer size){
+		List<Contract> list = null;
+		try {
+			contractMapper = (ContractMapper) setUp(ContractMapper.class);
+			Map<String,Object> map = new HashMap<String,Object>();
+			for(Map.Entry<String,Object> column : model.getMap().entrySet()){
+				if(column.getValue()!=null){
+					map.put(column.getKey(), column.getValue());
+				}
+			}
+			if(start!=null && size!=null){
+				map.put("start", start);
+				map.put("size", size);
+			}
+			list = contractMapper.findG_L(map);
+			tearDown();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+	
+	public List<Contract> findGe_L(Contract model,Integer start,Integer size){
+		List<Contract> list = null;
+		try {
+			contractMapper = (ContractMapper) setUp(ContractMapper.class);
+			Map<String,Object> map = new HashMap<String,Object>();
+			for(Map.Entry<String,Object> column : model.getMap().entrySet()){
+				if(column.getValue()!=null){
+					map.put(column.getKey(), column.getValue());
+				}
+			}
+			if(start!=null && size!=null){
+				map.put("start", start);
+				map.put("size", size);
+			}
+			list = contractMapper.findGe_L(map);
+			tearDown();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	public List<Contract> findG_Le(Contract model,Integer start,Integer size){
+		List<Contract> list = null;
+		try {
+			contractMapper = (ContractMapper) setUp(ContractMapper.class);
+			Map<String,Object> map = new HashMap<String,Object>();
+			for(Map.Entry<String,Object> column : model.getMap().entrySet()){
+				if(column.getValue()!=null){
+					map.put(column.getKey(), column.getValue());
+				}
+			}
+			if(start!=null && size!=null){
+				map.put("start", start);
+				map.put("size", size);
+			}
+			list = contractMapper.findG_Le(map);
+			tearDown();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+	
+
+	public List<Contract> findGe_Le(Contract model,Integer start,Integer size){
+		List<Contract> list = null;
+		try {
+			contractMapper = (ContractMapper) setUp(ContractMapper.class);
+			Map<String,Object> map = new HashMap<String,Object>();
+			for(Map.Entry<String,Object> column : model.getMap().entrySet()){
+				if(column.getValue()!=null){
+					map.put(column.getKey(), column.getValue());
+				}
+			}
+			if(start!=null && size!=null){
+				map.put("start", start);
+				map.put("size", size);
+			}
+			list = contractMapper.findGe_Le(map);
+			tearDown();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+	
 	public List<Contract> find(Contract model,Integer start,Integer size){
 		List<Contract> list = null;
 		try {
--- src/com/mangement/mybatis/mappers/ContractMapper.java
+++ src/com/mangement/mybatis/mappers/ContractMapper.java
@@ -12,5 +12,9 @@ public interface ContractMapper {
 	public void delete(Map<String,Object> map);
 	public List<Contract> find(Map<String,Object> map);
 	public List<Contract> findByID(String contractID);
+	public List<Contract> findG_L(Map<String,Object> map);
+	public List<Contract> findGe_L(Map<String,Object> map);
+	public List<Contract> findG_Le(Map<String,Object> map);
+	public List<Contract> findGe_Le(Map<String,Object> map);
 	public void update(Object object);
 }
